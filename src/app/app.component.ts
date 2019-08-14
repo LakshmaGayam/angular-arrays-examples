ae40ba5f8782cfdabab53ed237d1dabe9eaf4162
LakshmaGayam <lakshmagayam@gmail.com>
2019-08-14 12:12:08 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ export class AppComponent  {
   name = 'Angular';
    north:string[]=["delhi","pune","mumbai"]
      south:string[]=["AndhraPradesh","Telangana","Karnataka"]
-     cities:string[]=this.north.concat(this.south)
+     cities:string[]=this.north.concat(this.south).sort();
+
      a:number=2;
      b:number=3;
     c=this.a++;
