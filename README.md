d730bcc59e171d1ebe4fc071762d82c9a0923654
LakshmaGayam <lakshmagayam@gmail.com>
2019-08-14 12:10:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zyki8w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zyki8w)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+   north:string[]=["delhi","pune","mumbai"]
+     south:string[]=["AndhraPradesh","Telangana","Karnataka"]
+     cities:string[]=this.north.concat(this.south)
+     a:number=2;
+     b:number=3;
+    c=this.a++;
+ 
+ 
+}
